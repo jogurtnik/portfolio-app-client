c96e2be047f1fa09c6a16a27b17cb818dd056689
jogurtnik <remigiusz.widz@gmail.com>
2019-09-30 18:19:09 +0100

    Created Home container

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import "./Home.css"
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Remigiusz Widz</h1>
+          <p>Self-taught software developer</p>
+        </div>
+      </div>
+    )
+  }
+}
